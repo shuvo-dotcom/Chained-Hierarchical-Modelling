10b7f430b57e4ddc3e19b0490cb4d53766485c6b
Suvajit Lodh <x23356987@student.ncirl.ie>
2025-03-24 15:07:09 +0000

    Add files via upload

---
+++ modelling/data_model.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from Config import *
+import random
+seed =0
+random.seed(seed)
+np.random.seed(seed)
+
+class Data():
+    def __init__(self,
+                 X: np.ndarray,
+                 df: pd.DataFrame) -> None:
+
+        y = df.y.to_numpy()
+        y_series = pd.Series(y)
+
+        good_y_value = y_series.value_counts()[y_series.value_counts() >= 3].index
+
+        if len(good_y_value)<1:
+            print("None of the class have more than 3 records: Skipping ...")
+            self.X_train = None
+            return
+
+        y_good = y[y_series.isin(good_y_value)]
+        X_good = X[y_series.isin(good_y_value)]
+
+        # Label encode the target variable
+        self.label_encoder = LabelEncoder()
+        y_encoded = self.label_encoder.fit_transform(y_good)
+
+        new_test_size = X.shape[0] * 0.2 / X_good.shape[0]
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X_good, y_encoded, test_size=new_test_size, random_state=0, stratify=y_encoded)
+        self.y = y_encoded
+        self.classes = good_y_value
+        self.embeddings = X
+
+    def get_type(self):
+        return  self.y
+    def get_X_train(self):
+        return  self.X_train
+    def get_X_test(self):
+        return  self.X_test
+    def get_type_y_train(self):
+        return  self.y_train
+    def get_type_y_test(self):
+        return  self.y_test
+    def get_train_df(self):
+        return  self.train_df
+    def get_embeddings(self):
+        return  self.embeddings
+    def get_type_test_df(self):
+        return  self.test_df
+    def get_X_DL_test(self):
+        return self.X_DL_test
+    def get_X_DL_train(self):
+        return self.X_DL_train
+
+    def decode_predictions(self, predictions: np.ndarray) -> np.ndarray:
+        """
+        Convert encoded predictions back to original labels
+        :param predictions: Encoded predictions
+        :return: Original labels
+        """
+        return self.label_encoder.inverse_transform(predictions)
+
